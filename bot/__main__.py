11e3eed09363780859facce4ad542f087f3499ac
nonsecularman <krishbharti3404@gmail.com>
2026-02-05 02:05:23 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,9 +1,4 @@
-from pyrogram import idle
-from .config import Config
-from . import bot
-bot.start()
-if Config.PYRO_SESSION:
-   ass.start()
-idle()
-bot.stop()
+import asyncio
+from . import main
 
+asyncio.run(main())
